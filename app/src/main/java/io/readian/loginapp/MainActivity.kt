4492f6b4cc9cc7383474761230ace3459e662fc1
milasuguru <mirana.restless@gmail.com>
2021-12-12 14:10:15 +0300

    added login and sign up buttons

--- app/src/main/java/io/readian/loginapp/MainActivity.kt
+++ app/src/main/java/io/readian/loginapp/MainActivity.kt
@@ -2,10 +2,23 @@ package io.readian.loginapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-  }
+
+    private lateinit var logIn: Button
+    private lateinit var SignUp: Button
+    private lateinit var Skip: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        logIn = findViewById(R.id.button)
+
+        SignUp = findViewById(R.id.button2)
+
+        Skip = findViewById(R.id.button3)
+
+    }
 }
